ff49e6654076a6e953c3611ab1b2141100fb3308
aridevop <53029987+aridevop@users.noreply.github.com>
2019-08-02 12:49:14 +0530

    Add files via upload

---
+++ digswitch.php
@@ -0,0 +1,10 @@
+<?php
+// Swapping the first and last digits using php
+$p="12345";
+$len=strlen($p);
+$sst=substr($p,1,$len-2);
+$end=$p[0];
+$start=$p[$len-1];
+$totstr=$start.$sst.$end;
+echo "The reversed number is ".$totstr;
+?>
